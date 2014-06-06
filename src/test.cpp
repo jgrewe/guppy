3d1806cab9513512414e894695fe35d7fdb296d3
Jan Grewe <jan.grewe@uni-tuebingen.de>
2014-06-06 18:19:50 +0200

    running version with recording to file.

--- src/test.cpp
+++ src/test.cpp
@@ -9,40 +9,62 @@
 #include <iostream>
 #include <opencv2/highgui/highgui.hpp>
 #include "Guppy.hpp"
-
+#include <string>
+#include <boost/date_time/gregorian/gregorian.hpp>
 using namespace std;
 using namespace cv;
 
+string getDate(){
+  boost::gregorian::date current_date(boost::gregorian::day_clock::local_day());
+  return to_iso_extended_string(current_date);
+}
 
 int main() {
-	Guppy cam(0);
-	cam.exposure(250);
-	if (!cam.isOpened())  // if not success, exit program
-	{
-		cout << "Cannot open camera!" << endl;
-		return -1;
-	}
-
-	namedWindow("MyVideo",CV_WINDOW_AUTOSIZE); //create a window called "MyVideo"
-	int i = 0;
-	while (i < 250)
-	{
-		Mat frame;
-		bool bSuccess = cam.getFrame(frame); // read a new frame from video
-
-		if (!bSuccess) //if not success, break loop
-		{
-			cout << "Cannot read a frame from camera!!" << endl;
-			break;
-		}
-		imshow("MyVideo", frame); //show the frame in "MyVideo" window
-		if (waitKey(30) == 27) //wait for 'esc' key press for 30ms. If 'esc' key is pressed, break loop
-		{
-			cout << "esc key is pressed by user" << endl;
-			break;
-		}
-		i++;
-	}
-	cam.closeCam();
-	return 0;
+  int video_count = 0;
+  Guppy cam(0);
+  cam.exposure(250);
+  if (!cam.isOpened()) {
+    cout << "Cannot open camera!" << endl;
+    return -1;
+  }
+  int codec = CV_FOURCC('M', 'J', 'P', 'G');
+  Size frameSize(752, 580);
+  VideoWriter oVideoWriter;
+  namedWindow("MyVideo",CV_WINDOW_AUTOSIZE);
+  Mat frame;
+  bool recording = false;
+  while (true) {
+    bool bSuccess = cam.getFrame(frame);
+    if (!bSuccess) {
+      cout << "Cannot read a frame from camera!!" << endl;
+      break;
+    }
+    int key = waitKey(50);
+    if (key == 1048603) {
+      cout << "exit!" << endl;
+      break;
+    }
+    else if (key == 1048608) {
+      if (!recording) {
+	oVideoWriter.open(getDate() + "_" + to_string(video_count) + ".avi", codec, 20, frameSize, false);
+	cout << "recording started..." << endl;
+	recording = true;
+	if ( !oVideoWriter.isOpened() )
+	  {
+	    cout << "ERROR: Failed to write the video" << endl;
+	    return -1;
+	  }
+	video_count ++;
+      } else {
+	recording = false;
+	cout << "recording stopped!" << endl;
+      }
+    }
+    if(recording && oVideoWriter.isOpened()) {
+      oVideoWriter.write(frame); 
+    }
+    imshow("MyVideo", frame); 
+  }
+  cam.closeCam();
+  return 0;
 }
