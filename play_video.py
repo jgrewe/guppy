1e867e91f7092efb7faf92f62a6555fe96f04697
Jan Grewe <jan.grewe@g-node.org>
2014-08-20 17:04:50 +0200

    highlighting of tags during video playback

--- play_video.py
+++ play_video.py
@@ -10,11 +10,39 @@ import time
 import numpy as np
 from IPython import embed
 
+
 def findVideoArrayID(block):
     for a in block.data_arrays:
         if "nix.stamped_video" in a.type:
             return a.id
     return None
+
+
+def getTagPositions(block):
+    tag = None
+    for t in block.multi_tags:
+        if "nix.event" in t.type:
+            tag = t
+    if not tag:
+        return None
+
+    tag_times = tag.positions.data[tag.positions.data.shape[0]-1,:]
+    return tag_times
+
+
+def createTagMask(height, width, channels):
+    mask = np.zeros((height, width, channels))
+    cv2.rectangle(mask, (2,2), (width-2, height-2), (0, 255, 0), 4)
+    return mask
+
+
+def getTagFrames(ticks, positions):
+    tag_on = np.zeros((len(ticks)))
+    if positions is not None:
+        for p in positions:
+            temp = abs(ticks -p)
+            tag_on[(temp) == min(temp)] = 1
+    return tag_on
 
 
 if __name__ == '__main__':
@@ -45,14 +73,24 @@ if __name__ == '__main__':
     intervals = np.diff(ticks)
     intervals = np.hstack((intervals, np.mean(intervals)))
 
-    height, width, channel, nframes = da.data.shape
+    height, width, channels, nframes = da.data.shape
+
+    tag_mask = createTagMask(height, width, channels)
+    no_tag_mask = np.zeros((height, width, channels))
+    tag_positions = getTagPositions(block);
+    tag_on = getTagFrames(ticks, tag_positions)
+
     begin = time.time()
     for k in range(nframes):
         start = time.time()
         frame = da.data[:, :, :, k]
         frame = frame/256.
+        if tag_on[k] == 1:
+            img = cv2.add(frame,tag_mask)
+        else:
+            img = cv2.add(frame, no_tag_mask)
         #gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-        cv2.imshow('frame', frame)
+        cv2.imshow('frame', img)
         wait_interval = np.max((1, int(intervals[k] - (time.time() - start)*1000)))
         if cv2.waitKey(wait_interval) & 0xFF == ord('q'):
             break
